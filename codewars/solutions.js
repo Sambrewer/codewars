3d067c894ba3e7ee5fed58f79e28fb5059467168
Sam Brewer <samuelbrewer13@gmail.com>
2017-12-20 16:43:09 -0700

    I spy

--- codewars/solutions.js
+++ codewars/solutions.js
@@ -222,3 +222,24 @@ function perimeter(n) {
   }
     return sides * 4
 }
+
+// I Spy
+
+function spyOn(func) {
+  let calls = 0
+  let all = []
+  let val
+
+  const spy = function(...args) {
+    calls++
+    all.push(...args)
+    val = func.apply(this, args)
+    return val
+  }
+
+  spy.callCount = () => calls
+  spy.wasCalledWith = (x) => all.some((a) => x === a)
+  spy.returned = (x) => x === val
+
+  return spy
+}
