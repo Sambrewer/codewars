5c8def24858064bdf513228e5b5c229169994fe5
Samuelbrewer13 <samuel.brewer@improving.com>
2023-03-28 10:02:46 -0600

    Human Years Cat Years Dog Years

---
+++ solutions.ts
@@ -0,0 +1,21 @@
+// Human Years Cat Years Dog Years
+export function humanYearsCatYearsDogYears(humanYears: number): [number, number, number] {
+    // Your code here!
+    let catYears, dogYears;
+    switch (humanYears) {
+      case 1:
+        catYears = 15;
+        dogYears = 15;
+        break;
+      case 2:
+        catYears = 24
+        dogYears = 24; 
+        break;
+      default:
+        const over2 = humanYears - 2;
+        catYears = 24 + (over2 * 4);
+        dogYears = 24 + (over2 * 5);
+        break;
+    }
+    return [humanYears,catYears,dogYears];
+  }
